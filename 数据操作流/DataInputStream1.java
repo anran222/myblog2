abdbc019d6ab8130d5f242dcbb045a1078e5ff52
anran222 <1432085874@qq.com>
2020-01-04 22:50:39 +0800

    DataInputStream类

---
+++ 数据操作流/DataInputStream1.java
@@ -0,0 +1,37 @@
+package git;
+
+import java.io.FileInputStream;
+import java.io.File;
+import java.io.DataInputStream;
+
+public class DataInputStream1 {
+    public static void main(String[] args) throws Exception {
+        DataInputStream dis=null;
+        File f=new File("d："+File.separator+"order.txt");
+        dis =new DataInputStream(new FileInputStream(f));
+        String name=null;
+        float price=0.0f;
+        int num=0;
+        char temp[]=null;
+        int len=0;
+        char c=0;
+        try{
+            while(true){
+                temp=new char[200];
+                len=0;
+                while((c=dis.readChar())!='\t'){
+                    temp[len]=c;
+                    len++;
+                }
+                name=new String(temp,0,len);
+                price=dis.readFloat();
+                dis.readChar();
+                num=dis.readInt();
+                dis.readChar();
+                System.out.printf("名称:%s;价格:%5.2f,数量:%d\n",name,price,num);
+            }
+        }
+        catch(Exception e){ }
+        dis.close();
+    }
+}
